a42900dc46140a5de70da8a267fdf33c3aa89258
Zion Sheng <zionsheng@yahoo.com>
2025-04-10 22:42:01 -0400

    Minor change

--- zion_scripts/constants.py
+++ zion_scripts/constants.py
@@ -1,3 +1,4 @@
 WANDB_KEY = "be8a54927750ad82265d1afe8364759b899556f4"
 WANDB_ENTITY = "zionsheng-duke-university"
 WANDB_PROJECT_NAME = "segment-neuron-cell"
+CLASS_LABELS = {0: "background", 1: "neuron cell"}
